8cce577a50996f9e5c441944eefcb70199d5bf5b
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-05 22:41:31 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,22 +3,28 @@
 #include <string.h>
 #include "checker.h"
 
-void temperature_check(float temperature) {
+int temperature_check(float temperature) {
   if(temperature < TEMP_MINTHRESHOLD || temperature > TEMP_MAXTHRESHOLD) {
     printAlertMsg("Temperature");
+    return 0;
   }
+  return 1;
 }
 
-void SOC_check(float soc) {
+int SOC_check(float soc) {
   if(soc < SOC_MINTHRESHOLD || soc > SOC_MAXTHRESHOLD) {
     printAlertMsg("State of Charge");
+    return 0;
   }
+  return 1;
 }
 
-void chargeRate_check(float chargeRate) {
+int chargeRate_check(float chargeRate) {
   if(chargeRate > CHARGERATE_MAXTHRESHOLD) {
     printAlertMsg("Charge Rate");
+    return 0;
   }
+  return 1;
 }
 
 void printAlertMsg(string msg){
@@ -26,15 +32,13 @@ void printAlertMsg(string msg){
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  temperature_check(temperature);
-  SOC_check(soc);
-  chargeRate_check(chargeRate);
-  return 1;
+  int ret_value = 1;
+  ret_value = temperature_check(temperature)||SOC_check(soc)||chargeRate_check(chargeRate);
+  return ret_value;
 }
-
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(40, 85, 0));
 }
