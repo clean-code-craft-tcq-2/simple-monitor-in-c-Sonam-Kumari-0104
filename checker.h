f32689b9e350d2d76ada3d012ca06c0149d4ae1f
Sonam-Kumari-0104 <91051165+Sonam-Kumari-0104@users.noreply.github.com>
2022-02-08 20:05:19 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -5,3 +5,10 @@
 #define SOC_MAXTHRESHOLD 80
 #define CHARGERATE_MINTHRESHOLD 0.0
 #define CHARGERATE_MAXTHRESHOLD 0.8
+
+/*function Prototypes*/
+int BMSValuesoutOfRangeCheck(float input, float minThreshold, float maxThreshold);
+int temperature_check(float temperature);
+int SOC_check(float soc);
+int chargeRate_check(float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate) ;
